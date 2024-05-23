1a62d492eca1db57cf1760eb8dd6e84c0116d0c2
JUnseop Cho <wnstjq0704@naver.com>
2024-05-23 17:23:15 +0900

    second commit

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ from typing import List
 
 app = FastAPI()
 
-class Location(BaseModel):
-    id: int
-    name: str
-    tag: str
-    good: int
-    
+# class Location(BaseModel):
+#     id: int
+#     name: str
+#     tag: str
+#     good: int
+
 
 db = []
 
 @app.get("/")
-async def m():
-    return '안녕하세요'
+async def message():
+    return '어디가유 데이터 서버입니당'
 
-@app.post("/locations/", response_model=Location)
-async def create_location(location: Location):
-    db.append(location)
+@app.post("/locations/")
+async def create_location(location: list):
+    db.extend(location)
     return location
 
-@app.get("/locations/", response_model=List[Location])
+@app.get("/locations/")
 async def read_location():
     return db
 
-@app.get("/locations/{location_id}",response_model=Location)
-async def read_location(location_id: int):
+@app.get("/locations/{location_id}")
+async def read_location(location_name: str):
     for location in db:
-        if location.id == location_id:
+        if location['name'] == location_name:
             return location
 
