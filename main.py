2d5d207792451a2d181ce64e65330f4bc73a16c8
JUnseop Cho <wnstjq0704@naver.com>
2024-05-26 19:48:47 +0900

    description추가

--- main.py
+++ main.py
@@ -9,22 +9,23 @@ class Spot(BaseModel):
     name: str = None
     location: str = None
     time: str = None
-    tag: str = None
+    tags: str = None
+    description: str = None
     good: int = None
 
 
 db = []
 
-@app.get("/main")
+@app.get("/")
 async def message():
-    return '어디가유 데이터 서버입니당(spot)'
+    return '어디가유 데이터 서버입니당 spots'
 
 @app.post("/spots/")
 async def create_spot(spot: Spot):
     db.append(spot)
     return spot
 
-@app.get("/spots/")
+@app.get("/spots")
 async def read_spot():
     return db
 
